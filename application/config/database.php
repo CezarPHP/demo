6039f590597693232e4b3de8424f5abd69974d9a
cezar <cezar.ciubancan@icloud.com>
2015-05-11 00:27:25 +0300

    db credentials

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'jordy';
+$db['default']['username'] = 'greatza_jordy';
+$db['default']['password'] = '}4]8dL9Zd]kd';
+$db['default']['database'] = 'greatza_jordy';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
